b1be51102baded4f0e7211d11cf2f1e14ca33b4c
Rishi Sarkar <rishisarkar273@gmail.com>
2025-02-14 02:01:15 -0500

    Initial Commit

---
+++ OdysseyTests/OdysseyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyTests.swift
+//  OdysseyTests
+//
+//  Created by Rishi Sarkar on 2025-02-14.
+//
+
+import Testing
+@testable import Odyssey
+
+struct OdysseyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
